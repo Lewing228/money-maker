2b59cc4fc5ebaa7ca1e7e2a9528f684ff20d58ab
Danil Rumyancev <rumyancev_danil@mail.ru>
2024-05-15 03:34:29 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
-import Button from "./components/button";
+import FoodCard from "./components/Card/foodCard";
+import burgerImage from './images/burger.png';
+
 
 function App() {
-  return (
-    <div className="App">
-      <Button>refcdre</Button>
-    </div>
-  );
+    const cards = [
+        {
+            name: "Чизбургер",
+            weight: "310 г",
+            price: "430₽",
+            imageUrl: burgerImage
+        },
+        {
+            name: "Двойной Бургер",
+            weight: "500 г",
+            price: "530₽",
+            imageUrl: burgerImage
+        },
+        {
+            name: "Фиш Бургер",
+            weight: "350 г",
+            price: "450₽",
+            imageUrl: burgerImage
+        },
+        {
+            name: "Вегетарианский Бургер",
+            weight: "300 г",
+            price: "400₽",
+            imageUrl: burgerImage
+        }
+    ];
+
+    return (
+        <div className="App">
+            <div className="card-container">
+                {cards.map(card => (
+                    <FoodCard
+                        key={card.name}
+                        name={card.name}
+                        weight={card.weight}
+                        price={card.price}
+                        imageUrl={card.imageUrl}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Card/foodCard.jsx
+++ src/components/Card/foodCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import './FoodCard.css';  // Импорт стилей
+import './foodCard.css';
+import Button from "../Button/button";  // Импорт стилей
 
 const FoodCard = ({ name, weight, price, imageUrl }) => {
     return (
         <div className="food-card-container">
-            <div className="food-card-backdrop"></div>
-            <div className="food-card-price-container">
-                <div className="food-card-price"></div>
-                <div className="food-card-price-text">{price}</div>
+            <div className="food-card-backdrop">
+                <img className="food-card-image" src={imageUrl} alt="Food"/>
+                <div className="food-card-price-container">
+                    <div className="food-card-name">{name}</div>
+                    <div className="food-card-weight">{weight}</div>
+                    <Button price={price}/>
+                </div>
             </div>
-            <div className="food-card-weight">{weight}</div>
-            <div className="food-card-name">{name}</div>
-            <img className="food-card-image" src={imageUrl} alt="Food" />
         </div>
     );
 };
