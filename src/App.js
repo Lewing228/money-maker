1ab8aee5b1730a1c3cc759bdcbd0ca231e17b3c2
Danil Rumyancev <rumyancev_danil@mail.ru>
2024-05-14 19:09:01 +0500

    button

--- src/App.js
+++ src/App.js
@@ -1,95 +1,10 @@
 import './App.css';
+import Button from "./components/button";
 
 function App() {
   return (
     <div className="App">
-      <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
-    <img style={{width: 706, height: 1527, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/706x1527" alt='fff'/>
-    <div style={{width: 320, height: 473, left: 360, top: 1429, position: 'absolute'}}>
-        <div style={{width: 320, height: 473, left: 0, top: 0, position: 'absolute', background: 'rgba(255, 255, 255, 0.10)', borderRadius: 30, border: '1px #D6C27B solid'}} />
-        <div style={{width: 300, height: 60, left: 10, top: 403, position: 'absolute'}}>
-            <div style={{width: 300, height: 60, left: 0, top: 0, position: 'absolute', background: '#D9C77C', boxShadow: '0px 4px 5px rgba(255, 255, 255, 0.05)', borderRadius: 30}} />
-            <div style={{left: 122, top: 17, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 22, fontFamily: 'SF Pro Text', fontWeight: '500', wordWrap: 'break-word'}}>220₽</div>
-        </div>
-        <div style={{left: 20, top: 363, position: 'absolute', textAlign: 'center', color: 'rgba(255, 255, 255, 0.50)', fontSize: 17, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>190 г</div>
-        <div style={{width: 290, height: 23, left: 20, top: 330, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>Тосты</div>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 30}} src="https://via.placeholder.com/300x300" alt='fff'/>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 30}} src="https://via.placeholder.com/300x300" alt='fff'/>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 20}} src="https://via.placeholder.com/300x300" alt='fff'/>
-    </div>
-    <div style={{width: 320, height: 473, left: 26, top: 1429, position: 'absolute'}}>
-        <div style={{width: 320, height: 473, left: 0, top: 0, position: 'absolute', background: 'rgba(255, 255, 255, 0.10)', borderRadius: 30, border: '1px #D6C27B solid'}} />
-        <div style={{width: 300, height: 60, left: 10, top: 403, position: 'absolute'}}>
-            <div style={{width: 300, height: 60, left: 0, top: 0, position: 'absolute', background: '#D9C77C', boxShadow: '0px 4px 5px rgba(255, 255, 255, 0.05)', borderRadius: 30}} />
-            <div style={{left: 121, top: 17, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 22, fontFamily: 'SF Pro Text', fontWeight: '500', wordWrap: 'break-word'}}>640₽</div>
-        </div>
-        <div style={{left: 20, top: 363, position: 'absolute', textAlign: 'center', color: 'rgba(255, 255, 255, 0.50)', fontSize: 17, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>450 г</div>
-        <div style={{width: 290, height: 23, left: 20, top: 330, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>Ассорти из шашлыка</div>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 30}} src="https://via.placeholder.com/300x300" alt='fff'/>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 20}} src="https://via.placeholder.com/300x300" alt='fff'/>
-    </div>
-    <div style={{width: 320, height: 473, left: 360, top: 936, position: 'absolute'}}>
-        <div style={{width: 320, height: 473, left: 0, top: 0, position: 'absolute', background: 'rgba(255, 255, 255, 0.10)', borderRadius: 30, border: '1px #D6C27B solid'}} />
-        <div style={{width: 300, height: 60, left: 10, top: 403, position: 'absolute'}}>
-            <div style={{width: 300, height: 60, left: 0, top: 0, position: 'absolute', background: '#D9C77C', boxShadow: '0px 4px 5px rgba(255, 255, 255, 0.05)', borderRadius: 30}} />
-            <div style={{left: 121, top: 17, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 22, fontFamily: 'SF Pro Text', fontWeight: '500', wordWrap: 'break-word'}}>380₽</div>
-        </div>
-        <div style={{left: 20, top: 363, position: 'absolute', textAlign: 'center', color: 'rgba(255, 255, 255, 0.50)', fontSize: 17, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>270 г</div>
-        <div style={{width: 290, height: 23, left: 20, top: 330, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>Поке с лососем</div>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 30}} src="https://via.placeholder.com/300x300" alt='fff'/>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 30}} src="https://via.placeholder.com/300x300" alt='fff'/>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 20}} src="https://via.placeholder.com/300x300" alt='fff'/>
-    </div>
-    <div style={{width: 320, height: 473, left: 26, top: 936, position: 'absolute'}}>
-        <div style={{width: 320, height: 473, left: 0, top: 0, position: 'absolute', background: 'rgba(255, 255, 255, 0.10)', borderRadius: 30, border: '1px #D6C27B solid'}} />
-        <div style={{width: 300, height: 60, left: 10, top: 403, position: 'absolute'}}>
-            <div style={{width: 300, height: 60, left: 0, top: 0, position: 'absolute', background: '#D9C77C', boxShadow: '0px 4px 5px rgba(255, 255, 255, 0.05)', borderRadius: 30}} />
-            <div style={{left: 121, top: 17, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 22, fontFamily: 'SF Pro Text', fontWeight: '500', wordWrap: 'break-word'}}>440₽</div>
-        </div>
-        <div style={{left: 20, top: 363, position: 'absolute', textAlign: 'center', color: 'rgba(255, 255, 255, 0.50)', fontSize: 17, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>220 г</div>
-        <div style={{width: 290, height: 23, left: 20, top: 330, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>Сырный крем-суп</div>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 30}} src="https://via.placeholder.com/300x300" alt='fff'/>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 20}} src="https://via.placeholder.com/300x300" alt='fff'/>
-    </div>
-    <div style={{width: 320, height: 473, left: 360, top: 443, position: 'absolute'}}>
-        <div style={{width: 320, height: 473, left: 0, top: 0, position: 'absolute', background: 'rgba(255, 255, 255, 0.10)', borderRadius: 30, border: '1px #D6C27B solid'}} />
-        <div style={{width: 300, height: 60, left: 10, top: 403, position: 'absolute'}}>
-            <div style={{width: 300, height: 60, left: 0, top: 0, position: 'absolute', background: '#D9C77C', boxShadow: '0px 4px 5px rgba(255, 255, 255, 0.05)', borderRadius: 30}} />
-            <div style={{left: 121, top: 17, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 22, fontFamily: 'SF Pro Text', fontWeight: '500', wordWrap: 'break-word'}}>430₽</div>
-        </div>
-        <div style={{width: 43, height: 20, left: 20, top: 363, position: 'absolute', textAlign: 'center', color: 'rgba(255, 255, 255, 0.50)', fontSize: 17, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>310 г</div>
-        <div style={{width: 290, height: 23, left: 20, top: 330, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>Чизбургер</div>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 30}} src="https://via.placeholder.com/300x300" alt='fff'/>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 20}} src="https://via.placeholder.com/300x300" alt='fff'/>
-    </div>
-    <div style={{width: 320, height: 473, left: 26, top: 443, position: 'absolute'}}>
-        <div style={{width: 320, height: 473, left: 0, top: 0, position: 'absolute', background: 'rgba(255, 255, 255, 0.10)', borderRadius: 30, border: '1px #D6C27B solid'}} />
-        <div style={{width: 300, height: 60, left: 10, top: 403, position: 'absolute'}}>
-            <div style={{width: 300, height: 60, left: 0, top: 0, position: 'absolute', background: '#D9C77C', boxShadow: '0px 4px 5px rgba(255, 255, 255, 0.05)', borderRadius: 30}} />
-            <div style={{left: 122, top: 17, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 22, fontFamily: 'SF Pro Text', fontWeight: '500', wordWrap: 'break-word'}}>230₽</div>
-        </div>
-        <div style={{width: 43, height: 20, left: 20, top: 363, position: 'absolute', textAlign: 'center', color: 'rgba(255, 255, 255, 0.50)', fontSize: 17, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>190 г</div>
-        <div style={{width: 290, height: 23, left: 20, top: 330, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'SF Pro Text', fontWeight: '400', wordWrap: 'break-word'}}>Цезарь с курицей</div>
-        <img style={{width: 300, height: 300, left: 10, top: 10, position: 'absolute', background: 'linear-gradient(0deg, #D9D9D9 0%, #D9D9D9 100%)', borderRadius: 20}} src="https://via.placeholder.com/300x300" alt='fff'/>
-    </div>
-    <div style={{width: 669, left: 26, top: 350, position: 'absolute'}}>
-        <div style={{left: 220, top: 11, position: 'absolute', color: 'rgba(255, 255, 255, 0.50)', fontSize: 26, fontFamily: 'SF Pro Text', fontWeight: '300', wordWrap: 'break-word'}}>Салаты</div>
-        <div style={{left: 340, top: 11, position: 'absolute', color: 'rgba(255, 255, 255, 0.50)', fontSize: 26, fontFamily: 'SF Pro Text', fontWeight: '300', wordWrap: 'break-word'}}>Закуски</div>
-        <div style={{left: 467, top: 11, position: 'absolute', color: 'rgba(255, 255, 255, 0.50)', fontSize: 26, fontFamily: 'SF Pro Text', fontWeight: '300', wordWrap: 'break-word'}}>Супы</div>
-        <div style={{left: 558, top: 11, position: 'absolute', color: 'rgba(255, 255, 255, 0.50)', fontSize: 26, fontFamily: 'SF Pro Text', fontWeight: '300', wordWrap: 'break-word'}}>Десерты</div>
-        <div style={{width: 195, height: 53, left: 0, top: 0, position: 'absolute'}}>
-            <div style={{width: 195, height: 53, left: 0, top: 0, position: 'absolute', background: 'rgba(255, 255, 255, 0.10)', borderRadius: 20}} />
-            <div style={{left: 20, top: 11, position: 'absolute', textAlign: 'center', color: '#D6C27B', fontSize: 26, fontFamily: 'SF Pro Text', fontWeight: '300', wordWrap: 'break-word'}}>Популярное</div>
-        </div>
-    </div>
-    <div style={{width: 59, height: 59, left: 621, top: 235, position: 'absolute'}}>
-        <div style={{width: 12.29, height: 19.67, left: 19.67, top: 19.67, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 44, border: '1.50px #D9C77C solid'}}></div>
-        <div style={{width: 0, height: 4.92, left: 36.88, top: 29.50, position: 'absolute', borderRadius: 2, border: '1.50px #D9C77C solid'}}></div>
-        <div style={{width: 0, height: 4.92, left: 22.12, top: 29.50, position: 'absolute', borderRadius: 2, border: '1.50px #D9C77C solid'}}></div>
-        <div style={{width: 39.33, height: 31.96, left: 9.83, top: 19.67, position: 'absolute', border: '1.50px #D9C77C solid'}}></div>
-    </div>
-    <img style={{width: 90, height: 90, left: 308, top: 220, position: 'absolute'}} src="https://via.placeholder.com/90x90" alt='fff'/>
-</div>
+      <Button>refcdre</Button>
     </div>
   );
 }
