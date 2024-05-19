f2e51ac56e7769d918c784e59b90ce671a3ffa1e
Danil Rumyancev <rumyancev_danil@mail.ru>
2024-05-19 12:18:12 +0500

    commit

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import './button.css'
+// Button.js
+import React, { useState } from 'react';
+import './button.css';
 
 const Button = ({ price, label, size, classLabel, onClick }) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(true);
+        if (onClick) {
+            onClick();
+        }
+    };
+
     return (
-        <button className={`button ${label && price ? 'button-with-label' : ''} ${size}`} onClick={onClick}>
+        <button
+            className={`button ${label && price ? 'button-with-label' : ''} ${size} ${isClicked ? 'clicked' : ''}`}
+            onClick={handleClick}
+        >
             {label && <span className={`button-label ${classLabel}`}>{label}</span>}
             {price && <span className={`button-price ${label ? 'button-price-with-label' : ''}`}>{price}</span>}
         </button>
--- src/components/Card/foodCard.jsx
+++ src/components/Card/foodCard.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './foodCard.css';
 import { Link } from 'react-router-dom';
 
 const FoodCard = ({ id, name, weight, price, imageUrl, onAddToCart }) => {
+    const [isClicked, setIsClicked] = useState(false);
+
     const handleButtonClick = (e) => {
         e.preventDefault();  // Prevent the default action
         e.stopPropagation(); // Prevent the event from bubbling up to the parent elements
         onAddToCart();
+        setIsClicked(true); // Update the state to indicate the button has been clicked
     };
 
     return (
@@ -17,7 +20,9 @@ const FoodCard = ({ id, name, weight, price, imageUrl, onAddToCart }) => {
                     <div className="food-card-price-container">
                         <div className="food-card-name">{name}</div>
                         <div className="food-card-weight">{weight}</div>
-                        <button className="button" onClick={handleButtonClick}>{price}₽</button>
+                        <button className={`button ${isClicked ? 'clicked' : ''}`} onClick={handleButtonClick}>
+                            {price}₽
+                        </button>
                     </div>
                 </div>
             </div>
