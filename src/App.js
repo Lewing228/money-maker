cf18a9319da69c2627cafaa62c1b8a880bc83937
Danil Rumyancev <rumyancev_danil@mail.ru>
2024-06-11 15:10:54 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from "react";
 import Header from './components/header/header';
 import Casher from './components/casher/casher';
-import Paper from './components/paper/paper';
 import Footer from './components/footer/footer';
 function App() {
     // const app = window.Telegram?.WebApp; // Оставляем переменную закомментированной, если она не используется
