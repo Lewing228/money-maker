d1b7da4e2694cd56497660c10c48e011e0e77e49
Danil Rumyancev <rumyancev_danil@mail.ru>
2024-06-11 14:59:01 +0500

    commit

--- src/components/paper/paper.jsx
+++ src/components/paper/paper.jsx
@@ -1,32 +1,20 @@
 import React, { useState } from 'react';
 import './paper.css';
-import paper from '../../images/paper.png';
 import Popup from '../popup/popup';
 
 const Paper = ({ paperCount, maxPaperCount, onBuyPaper, onUpgradeMaxPaper }) => {
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-    const togglePopup = () => {
-        setIsPopupOpen(!isPopupOpen);
-    };
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
 
-    return (
-        <div className="paper_block">
-            <div className="paper_main_block">
-                <div className="paper_img"> 
-                    <img src={paper} alt="User Avatar" />
-                </div>
-                <div className="block-paper">
-                    <p className="paper_text">Paper</p>
-                    <p className='paper_buy' onClick={togglePopup}>Buy more</p>
-                </div>
-            </div>
-            <div className="paper_count"> 
-                <p className="paper_count_text">{paperCount.toFixed(1)}/{maxPaperCount}</p>
-            </div>
-            {isPopupOpen && <Popup onClose={togglePopup} onBuyPaper={onBuyPaper} onUpgradeMaxPaper={onUpgradeMaxPaper} />}
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={togglePopup}>Buy more paper</button>
+      {isPopupOpen && <Popup onClose={togglePopup} onBuyPaper={onBuyPaper} onUpgradeMaxPaper={onUpgradeMaxPaper} />}
+    </div>
+  );
 };
 
 export default Paper;
